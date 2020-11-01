396dd3f5442a78581e8fe259bfabfe739e5f3854
Prashant523580 <prashant.thapa62758@gmail.com>
2020-11-02 03:26:47 +0545

    check whether the given number is positive or negetive

--- conditions/neg_pos.py
+++ conditions/neg_pos.py
@@ -2,9 +2,15 @@
 ui = input("enter a number: ")
 ui = float(ui) #converting  it to a floating point number
 
+#if the number is greater then zero
+# output positive
 if ui > 0:
 	print("positive number")
+#if the number is less then zero 
+#output negative
 elif ui < 0:
-	print("negetive number")
+	print("negative number")
+#in all other case 
 else:
 	print("zero ")
+
