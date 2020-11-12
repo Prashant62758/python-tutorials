87b2cb10fdf78daaab5fff04fdcb37d8df80e085
prashant523580 <prashant.thapa62758@gmail.com>
2020-11-12 23:10:31 +0545

    named indexes method

--- string_formating.py
+++ string_formating.py
@@ -4,3 +4,6 @@ city = "Bharatpur"
 
 txt = "his name is {1}. {1} is {0} years old. {1} is from {2}"
 print(txt.format(age,name,city))
+#named indexes
+order = "i have a  {bikename} bike. It is a {model}"
+print(order.format(bikename="pulser", model=220))
